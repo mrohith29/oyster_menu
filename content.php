a67c8d9911d40936e51e8ab71904c48631b7819b
mrohith29 <mariyalarohith29@gmail.com>
2024-06-12 21:18:09 +0530

    correct strucutre of the html

--- content.php
+++ content.php
@@ -9,16 +9,21 @@ require_once __DIR__ . '/head.php';
 
 use \Approach\Render\HTML;
 
-$body->content[] = new HTML(tag: 'div', attributes: ['class' => 'Stage']);
-$body->content[] = new HTML(tag: 'div', attributes: ['id' => 'main', 'class' => 'Screen']);
-$body->content[] = new HTML(tag: 'div', attributes: ['class' => 'Oyster Interface controls animate__animated animate__fadeIn']);
-$body->content[] = new HTML(tag: 'section', attributes: ['class' => 'header']);
-$body->content[] = new HTML(tag: 'button', attributes: ['class' => 'backBtn']);
-$body->content[] = new HTML(tag: 'i', attributes: ['class' => 'expand fa fa-angle-left']);
-$body->content[] = new HTML(tag: 'button', attributes: ['class' => 'btn btn-secondary current-state ms-2 animate__animated animate__slideInDown', 'id' => 'menuButton']);
-$body->content[] = new HTML(tag: 'span', attributes: ['id' => 'menuButtonText'], content: 'Location');
-$body->content[] = new HTML(tag: 'i', attributes: ['class' => 'fa fa-caret-down']);
-$body->content[] = new HTML(tag: 'ul', attributes: ['class' => 'breadcrumbs', 'style' => 'display: none']);
-$body->content[] = new HTML(tag: 'ul', attributes: ['class' => 'Toolbar']);
-$body->content[] = new HTML(tag: 'div', attributes: ['class' => 'signOut']);
+$body[] = new HTML(tag: 'div');
+$stage[] = new HTML(tag:'div',attributes: ['class' => 'Stage']);
+$oyster[] = new HTML(tag: 'div', attributes: ['class' => 'Oyster Interface controls animate__animated animate__fadeIn']);
+$header[] = new HTML(tag: 'section', attributes: ['class' => 'header']);
+$bckBtn[] = new HTML(tag: 'button', attributes: ['class' => 'backBtn']);
+$expand = new HTML(tag: 'i', attributes: ['class' => 'fa fa-caret-down']);
+$bckBtn[] = $expand;
+$header[] = $bckBtn;
+$menuButton[] = new HTML(tag: 'button', attributes: ['class' => 'btn btn-secondary current-state ms-2 animate__animated animate__slideInDown', 'id' => 'menuButton']);
+$menuButtonText = new HTML(tag: 'span', attributes: ['id' => 'menuButtonText'], content: 'Location');
+$menuButton[] = $menuButtonText;
+$fa = new HTML(tag:'i', attributes: ['class' => 'fa fa-caret-down']);
+$menuButton[] = $fa;
+$header[] = $menuButton;
+$breadCrumbs = new HTML(tag: 'ul', attributes: ['class' => 'breadcrumbs', 'style' => 'display: none']);
+$header[] = $breadCrumbs;
 
+$oyster[] = $header;
